f8c68d9e280adc6f0c911d0494fdcecd8d8f590a
Nick Adams <nickadams@outlook.com>
2013-10-05 15:42:15 -0700

    adding validation

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ form = """
 	<br>
 	<br>
 	<label> Month
-	<input type = 'text' name = 'month' current_phase_filter[] = delivered>
+	<input type = 'text' name = 'month'>
 	</label>
 	<label> Day
 	<input type = 'text' name = 'day'>
@@ -24,9 +24,46 @@ class MainHandler(webapp2.RequestHandler):
     def get(self):
         self.response.out.write(form)
 
+    months = ['January',
+          'February',
+          'March',
+          'April',
+          'May',
+          'June',
+          'July',
+          'August',
+          'September',
+          'October',
+          'November',
+          'December']
+
+	def valid_month(month):
+	    if month:
+	        cap_month = month.capitalize()
+	        if cap_month in months:
+	            return cap_month
+
+	def valid_day(day):
+    	if day and day.isdigit():
+        	day = int(day)
+        	if day >= 1 and day<= 31:
+            	return day
+
+    def valid_year(year):
+    	if year and year.isdigit():
+        	if int(year) >= 1900 and int(year) <=2020:
+            	return int(year)
+
+
     def post(self):
-    	user_month = valid_month(self.request.get)
-    	self.response.out.write("Thanks! That's a totally valid day!")
+    	user_month = valid_month(self.request.get('month'))
+    	user_day = valid_day(self.request.get('day'))
+    	user_year = valid_year(self.request.get('year')
+
+    	if not (user_year and user_month and user_day):
+    		self.response.out.write(form)
+    	else:
+    		self.response.out.write("Thanks!  That's a totally vaild day")
 
 app = webapp2.WSGIApplication([
 	('/', MainHandler),
